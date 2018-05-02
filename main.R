a74ededa57164be1e751f29aacc7730d8f76ed10
Eloy Eligon <ee.eligon@gmail.com>
2018-05-02 07:46:44 -0700

    Improve user input

--- main.R
+++ main.R
@@ -7,8 +7,14 @@
 library(bittrex)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 
-N_SNAPSHOTS <- 500
+DATA_DIR <- 'data'
+PROCESSED_DATA_DIR <- file.path(DATA_DIR, 'processed')
+
+N_SNAPSHOTS <- 10
+INTERVAL <- 1
+FILE_NAME <- 'arbitrage_snapshots.csv'
 
 # Get markets ####
 check_for_arbitrage <- function(.threshold = 1.0075, .fee = .0025, .sound = TRUE) {
@@ -60,8 +66,11 @@ arbitrage_snapshots = data_frame(
 )
 
 for(i in 1:N_SNAPSHOTS) {
+  cat(sprintf('Working on iteration %s\n', i))
   arbitrage_snapshots <<- arbitrage_snapshots %>% bind_rows(check_for_arbitrage()$arbitrage_snapshot)
-  Sys.sleep(1)
+  Sys.sleep(INTERVAL)
 }
 
-write.csv('data/arbitrage_snapshots.csv', row.names = FALSE)
+cat('Exporting data')
+arbitrage_snapshots <- arbitrage_snapshots %>% mutate(timestamp = as_datetime(timestamp))
+write.csv(arbitrage_snapshots, file.path(PROCESSED_DATA_DIR, FILE_NAME), row.names = FALSE)
